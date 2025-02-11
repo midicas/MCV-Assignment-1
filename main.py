7025d88d8ef5491794ee44f8d3e7632995c67144
midicas <casbouwman02@gmail.com>
2025-02-11 11:31:28 +0100

    added image folder

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+
+coordinates = []
+def mouseClickEventHandler(event, x, y, flags, params):
+    if (event == cv2.EVENT_LBUTTONUP):
+        print(x, ' ', y) 
+        if len(coordinates) < 4:
+            coordinates.append((x,y))
+            cv2.circle(img, (x,y),5,(0, 255, 255), 2) 
+            cv2.imshow("image",img)
+
+
+if (__name__ == "__main__"):
+    img = cv2.imread("images/Original_lena512.jpg",1)
+    cv2.imshow("image",img)
+
+    cv2.setMouseCallback("image",mouseClickEventHandler)
+    cv2.waitKey(0) 
+  
+    # close the window 
+    cv2.destroyAllWindows() 
