185bd083d860000b6c9f87083fd8166a030902f8
midicas <95248199+midicas@users.noreply.github.com>
2025-02-12 16:01:43 +0100

    Added runs

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import numpy as np
 import os
 import sys
 
-PATTERN_SIZE = (9, 6)
+PATTERN_SIZE = (8, 5)
 
-SQUARE_SIZE = 1
+SQUARE_SIZE = 25
 
 def click_corners(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN and len(param) < 4:
-        param.append((x, y))
+    points = param
+    if event == cv2.EVENT_LBUTTONDOWN and len(points) < 4:
+        points.append((x, y))
 
 def manual_corner_selection(img):
     points = []
     clone = img.copy()
-    
+
     cv2.imshow("Pick 4 corners", clone)
     cv2.setMouseCallback("Pick 4 corners", click_corners, points)
     
@@ -26,15 +27,14 @@ def manual_corner_selection(img):
     topPoints = points[:2]
     topPoints = sorted(topPoints)
     bottomPoints = points[2:]
-    bottomPoints = sorted(bottomPoints, key = lambda coordinate:coordinate[1])
+    bottomPoints = sorted(bottomPoints)
     
     points = topPoints + bottomPoints
-    print(points)
     return np.array(points, dtype=np.float32)
 
-def interpolate_grid_corners(corners, pattern_size):
+def interpolate_grid_corners(corners):
     
-    rows, cols = pattern_size
+    rows, cols = PATTERN_SIZE
     grid_corners = np.zeros((rows * cols, 1, 2), dtype=np.float32)
 
     colMultiplier = 1/(cols-1)
@@ -50,25 +50,27 @@ def interpolate_grid_corners(corners, pattern_size):
             grid_corners[row * cols + col,0] = (1-rowMultiplier*row) * topInterpolation + rowMultiplier*row*bottomInterpolation
     return grid_corners
 
-def detectCorners(img, patternSize=(7, 7), show_result=False):
+def detectCorners(img, show_result=False):
     
-    passed, corners = cv2.findChessboardCorners(img, patternSize, None)
-
-    if not passed:
+    passed, corners = cv2.findChessboardCorners(img, PATTERN_SIZE, None)
+    
+    if passed:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1),criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
+    else:        
         outerCorners = manual_corner_selection(img)
-        corners = interpolate_grid_corners(outerCorners, patternSize)
+        corners = interpolate_grid_corners(outerCorners)
+
     
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1),
-                              criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
     if show_result:
-        cv2.drawChessboardCorners(img, patternSize, corners, True)
-        cv2.imshow("Detected corners", img)
+        clone = img.copy()
+        cv2.drawChessboardCorners(clone, PATTERN_SIZE, corners, True)
+        cv2.imshow("Detected corners", clone)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
+    
     return corners
-def run(folder):
+def calibrationRun(folder):
     images = [f for f in os.listdir(folder)]
     
     realWorldPoints = np.zeros((PATTERN_SIZE[0]*PATTERN_SIZE[1],3), np.float32)
@@ -77,25 +79,34 @@ def run(folder):
     realWorldCollection = []
     imagePoints = []
     for fname in images:
-        path = os.path.join(images,fname)
+        print(f"Processing Image: {fname}")
+        path = os.path.join(folder,fname)
         img = cv2.imread(path)
-        corners = detectCorners(img, PATTERN_SIZE)
+        corners = detectCorners(img,False)
 
         realWorldCollection.append(realWorldPoints)
         imagePoints.append(corners)
     
-    #Calibrate Camera
-    #return camera values
+    _, cameraMatrix, distCoeffs, _, _ = cv2.calibrateCamera(
+    realWorldCollection, imagePoints, img.shape[1::-1], None, None
+)
+    print("Camera Matrix:\n", cameraMatrix)
+    np.savez(f"{folder}.npz", 
+            camera_matrix=cameraMatrix, 
+            dist_coeffs=distCoeffs)
 
-    return None
+    return cameraMatrix,distCoeffs
 
-    
+def testImage(img,cameraMatrix,distCoeffs):
+    return
 
 
 
 if __name__ == "__main__":
-    img = cv2.imread("images/lena.jpg",1)
-    detectCorners(img,(9,6),True)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    #detectCorners()
+    folder_path = sys.argv[1]
+    cameraMatrix,distCoeffs = calibrationRun(folder_path)
+    testImg = cv2.imread("test_image.jpeg")
+    testImage(testImg,cameraMatrix,distCoeffs)
+
+
+    
